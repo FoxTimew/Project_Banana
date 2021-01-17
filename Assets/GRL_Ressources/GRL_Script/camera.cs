95443a87813d50bb823f72f4cda4872781c7db0e
Mynns <g.laroumanie@rubika-edu.com>
2021-01-17 15:59:45 +0100

    fin du hub et cam

---
+++ Assets/GRL_Ressources/GRL_Script/camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camera : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera shopVirtualCam;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("cam prete");
+        shopVirtualCam.Priority= -25;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        shopVirtualCam.Priority= 11;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
